1e6bfb4d4f3f01e369e0a11aadc8d75153046b8b
mica <mica.eldridge1@gmail.com>
2018-10-08 12:55:38 -0700

    change view.bash to center around a "view.txt" file instead of stdout piping, so viewing multiple times will result in one view instead of many copies of old views being left open in textmate windows...

--- view.bash
+++ view.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 this_dir="$(dirname "$(readlink -e "$0")")"
+
+view_txt="$this_dir"/view.txt
+
+./commands.py --view > "$view_txt"
+
 view_cmd=mate
 which $view_cmd >/dev/null || view_cmd=less
-./commands.py --view | "$view_cmd"
+"$view_cmd" "$view_txt"
